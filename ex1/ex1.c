a3ea30fa96a8a292f17b16c9905c6cbfa99e5149
MEBonham <programmer@mckaybonham.com>
2019-03-20 14:28:48 -0600

    Implements exercise1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -5,10 +5,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(void)
 {
     // Your code here
+    int x = 100;
+
+    pid_t pid = fork();
+
+    if (pid == 0) // Child process
+    {
+        printf("Initial value of x in child process: %d\n", x);
+        x = 99;
+        printf("Proof of change of value: %d\n", x);
+    }
+    else // Parent process
+    {
+        x = 5;
+        wait(NULL);
+        printf("x in child process after both changes: %d\n", x);
+    }
 
     return 0;
 }
